47e141bb7b3e092786ad3d9ce74dbddc586ffe30
LeyaThomas <leyathomas1352002@gmail.com>
2024-04-26 20:03:35 +0530

    submit setup

--- frontend/src/pages/UpdateHub/UpdateHub.jsx
+++ frontend/src/pages/UpdateHub/UpdateHub.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Typography, Radio, RadioGroup, FormControlLabel, FormControl, Button, Paper, Select, MenuItem, InputLabel, useTheme } from '@material-ui/core';
 import { tokens } from '../../Theme';
 import Header from '../../components/Header/Header.jsx';
@@ -8,66 +8,95 @@ const UpdateHub = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const [answers, setAnswers] = useState({
-        question1: '',
-        question2: '',
-        question3: '',
-        question4: '',
-        question5: '',
+        question1: null,
+        question2: null,
+        question3: null,
+        question4: null,
+        question5: null,
         // Add more questions as needed
     });
     const [allAnswered, setAllAnswered] = useState(false);
     const [projects, setProjects] = useState([]);
+    const [selectedProject, setSelectedProject] = useState(null);
 
     useEffect(() => {
         const employeeId = localStorage.getItem('cususerid');  // Fetch employee id from local storage
         if (employeeId) {
-          const url = `http://localhost:8000/projects/employee/${employeeId}/`;
-          axios.get(url)  // Use employee id in API URL
-            .then(response => {
-              setProjects(response.data);
-            })
-            .catch(error => {
-              console.error('There was an error!', error);
-            });
+            const url = `http://localhost:8000/projects/employee/${employeeId}/`;
+            axios.get(url)  // Use employee id in API URL
+                .then(response => {
+                    setProjects(response.data);
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+                });
         } else {
-          console.error('Employee ID is null');
+            console.error('Employee ID is null');
         }
-      }, []);
+    }, []);
 
 
     const handleChange = (event) => {
-        setAnswers({ ...answers, [event.target.name]: event.target.value });
-        const allQuestionsAnswered = Object.values(answers).every(answer => answer !== '');
-        setAllAnswered(allQuestionsAnswered);
+        const value = event.target.name === 'project' ? event.target.value : event.target.value === 'yes' ? true : false;
+        if (event.target.name === 'project') {
+            setSelectedProject(value);
+        } else {
+            setAnswers({
+                ...answers,
+                [event.target.name]: value,
+            });
+        }
     };
 
     const handleSubmit = () => {
         // Send answers to backend
-        console.log(answers);
+        const employeeId = localStorage.getItem('cususerid');  // Fetch employee id from local storage
+    
+        if (employeeId && selectedProject) {
+            const url = 'http://localhost:8000/projects/employeeanswer/';  // Use employee id in API URL
+            const data = {
+                ...answers,
+                employee: employeeId,  // Include employee id in the data
+                project: selectedProject,
+            };
+
+            console.log('Data sent to the server:', data);
+            axios.post(url, data)  // Send data to the backend
+                .then(response => {
+                    console.log('Answers sent successfully');
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+                    console.log('Error message:', error.message);
+                    if (error.response) {
+                        console.log('Server response:', error.response.data);
+                    }
+                });
+        }
     };
 
     return (
         <Box style={{ width: '90%', marginTop: '-140px' }}>
             <Header title="Updates Hub" variant="h1" style={{ fontSize: '5em', fontWeight: 'bold' }} />
             <FormControl>
-        <InputLabel id="project-select-label">Project</InputLabel>
-        <Select
-          labelId="project-select-label"
-          id="project-select"
-          value={answers.project}
-          onChange={handleChange}
-          name="project"
-        >
-          {projects.map((project) => (
-            <MenuItem key={project.id} value={project.id}>{project.name}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+                <InputLabel id="project-select-label">Project</InputLabel>
+                <Select
+                    labelId="project-select-label"
+                    id="project-select"
+                    value={selectedProject}
+                    onChange={handleChange}
+                    name="project"
+                >
+                    {projects.map((project) => (
+                        <MenuItem key={project.id} value={project.id}>{project.name}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
 
             <FormControl component="fieldset" style={{ width: '100%' }}>
                 <Paper elevation={3} style={{ padding: '10px', marginBottom: '10px', width: '100%', backgroundColor: colors.gray[100] }}>
                     <Typography variant="h6" style={{ color: colors.greenAccent[500] }}>Question 1</Typography>
-                    <RadioGroup name="question1" value={answers.question1} onChange={handleChange}>
+                    <RadioGroup name="question1" value={answers.question1 === null ? '' : answers.question1 ? 'yes' : 'no'} onChange={handleChange}>
                         <FormControlLabel value="yes" control={<Radio />} label="Yes" style={{ color: '#fff' }} />
                         <FormControlLabel value="no" control={<Radio />} label="No" style={{ color: '#fff' }} />
                     </RadioGroup>
@@ -76,7 +105,7 @@ const UpdateHub = () => {
 
                 <Paper elevation={3} style={{ padding: '10px', marginBottom: '10px', width: '100%', backgroundColor: colors.gray[100] }}>
                     <Typography variant="h6" style={{ color: colors.greenAccent[500] }}>Question 2</Typography>
-                    <RadioGroup name="question2" value={answers.question2} onChange={handleChange}>
+                    <RadioGroup name="question2" value={answers.question2 === null ? '' : answers.question2 ? 'yes' : 'no'} onChange={handleChange}>
                         <FormControlLabel value="yes" control={<Radio />} label="Yes" style={{ color: '#fff' }} />
                         <FormControlLabel value="no" control={<Radio />} label="No" style={{ color: '#fff' }} />
                     </RadioGroup>
@@ -85,7 +114,7 @@ const UpdateHub = () => {
 
                 <Paper elevation={3} style={{ padding: '10px', marginBottom: '10px', width: '100%', backgroundColor: colors.gray[100] }}>
                     <Typography variant="h6" style={{ color: colors.greenAccent[500] }}>Question 3</Typography>
-                    <RadioGroup name="question3" value={answers.question3} onChange={handleChange}>
+                    <RadioGroup name="question3" value={answers.question3 === null ? '' : answers.question3 ? 'yes' : 'no'} onChange={handleChange}>
                         <FormControlLabel value="yes" control={<Radio />} label="Yes" style={{ color: '#fff' }} />
                         <FormControlLabel value="no" control={<Radio />} label="No" style={{ color: '#fff' }} />
                     </RadioGroup>
@@ -94,7 +123,7 @@ const UpdateHub = () => {
 
                 <Paper elevation={3} style={{ padding: '10px', marginBottom: '10px', width: '100%', backgroundColor: colors.gray[100] }}>
                     <Typography variant="h6" style={{ color: colors.greenAccent[500] }}>Question 4</Typography>
-                    <RadioGroup name="question4" value={answers.question4} onChange={handleChange}>
+                    <RadioGroup name="question4" value={answers.question4 === null ? '' : answers.question4 ? 'yes' : 'no'} onChange={handleChange}>
                         <FormControlLabel value="yes" control={<Radio />} label="Yes" style={{ color: '#fff' }} />
                         <FormControlLabel value="no" control={<Radio />} label="No" style={{ color: '#fff' }} />
                     </RadioGroup>
@@ -103,7 +132,7 @@ const UpdateHub = () => {
 
                 <Paper elevation={3} style={{ padding: '10px', marginBottom: '10px', width: '100%', backgroundColor: colors.gray[100] }}>
                     <Typography variant="h6" style={{ color: colors.greenAccent[500] }}>Question 5</Typography>
-                    <RadioGroup name="question5" value={answers.question5} onChange={handleChange}>
+                    <RadioGroup name="question5" value={answers.question5 === null ? '' : answers.question5 ? 'yes' : 'no'} onChange={handleChange}>
                         <FormControlLabel value="yes" control={<Radio />} label="Yes" style={{ color: '#fff' }} />
                         <FormControlLabel value="no" control={<Radio />} label="No" style={{ color: '#fff' }} />
                     </RadioGroup>
