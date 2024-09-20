1809d1377e2b525ec4858a538c8da8a03352fdfc
LeyaThomas <leyathomas1352002@gmail.com>
2024-09-20 17:32:28 +0530

    /

--- src/pages/Signup/signup.jsx
+++ src/pages/Signup/signup.jsx
@@ -26,8 +26,9 @@ const SignUp = () => {
 
   useEffect(() => {
     const fetchCompanies = async () => {
+      
       try {
-        const response = await axios.get(process.env.REACT_APP_API_URL + '/users/companies/'); 
+        const response = await axios.get(process.env.REACT_APP_API_URL + '/users/companies'); 
         setCompanies(response.data);
       } catch (error) {
         console.error('Failed to fetch companies:', error);
